09fb1644319b2ac55474f9a3809c48bc50445163
nn16nn <nurbol123kmm@gmail.com>
2026-09-03 03:09:55 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'bolzham-v8';
+const V = 'bolzham-v9';
 const CORE = ['./', './index.html', './manifest.webmanifest',
               './icon-192.png', './icon-512.png', './icon-maskable.png'];
 
